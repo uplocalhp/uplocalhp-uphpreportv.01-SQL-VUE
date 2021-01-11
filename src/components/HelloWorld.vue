c1097fd2f903f3dccd319686ecf3a1d4ce8b6be6
MainCom <up.localhp@gmail.com>
2021-01-11 14:58:30 +0700

    master

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,71 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+
+  <v-app>
+    <v-container>
+    <v-form @submit.prevent="handleSubmit" v-model="valid"
+    >
+      <v-card
+          class="mx-auto my-12"
+          max-width="374"
+      >
+        <v-img
+            height="350"
+            src="https://upload.wikimedia.org/wikipedia/th/thumb/a/a4/UPHosLogo.svg/1024px-UPHosLogo.svg.png"
+        ></v-img>
+        <v-card-title>ระบบแจ้งซ่อม</v-card-title>
+          <div>
+              <v-card-text>
+                <v-text-field 
+                  v-model="username"
+                  label="Username"
+                  hide-details="auto"
+                  :rules="emailRules"
+                  required
+                ></v-text-field>
+                <v-text-field 
+                  v-model="password"
+                  label="Password" 
+                  type="password" 
+                  hide-details="auto"
+                  :rules="nameRules"
+                  required
+                ></v-text-field>
+              </v-card-text>
+              <v-card-actions >
+              <v-btn color="success" type="submit" value="submit" :disabled="!valid">login</v-btn>
+              </v-card-actions>
+          </div>
+      </v-card> 
+    </v-form>
+    
+    
+  </v-container>
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    data: () => ({
+      valid: true,
+      username :'',
+      password :'',
+      nameRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 6) || 'Password must be less than 6 characters',
+      ],
+       emailRules: [
+        v => !!v || 'Username is required',
+      ],
+    }),
+    methods: {
+      handleSubmit (e) {
+        e.preventDefault();
+        const { username, password } = this;
+        console.log(username,password);
+          this.login({username,password})
+      },
+    },
+  }
+</script>
